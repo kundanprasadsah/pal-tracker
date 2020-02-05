79a0a772690f963f09a1dbc90f53efed42987795
Bill Kable <bkable@gmail.com>
2020-02-05 00:41:11 -0600

    Lab 7 - Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,12 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -14,7 +18,11 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository inMemoryTimeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository jdbcTimeEntryRepository(){
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
